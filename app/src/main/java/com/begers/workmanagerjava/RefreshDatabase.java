c97c39360176dd33fca6acc3b242c51d48ba33e2
bekirgeris <bekirgeris@marun.edu.tr>
2021-11-15 11:54:31 +0300

    +worker class

--- app/src/main/java/com/begers/workmanagerjava/RefreshDatabase.java
+++ app/src/main/java/com/begers/workmanagerjava/RefreshDatabase.java
@@ -1,6 +1,7 @@
 package com.begers.workmanagerjava;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
@@ -8,14 +9,26 @@ import androidx.work.WorkerParameters;
 
 public class RefreshDatabase extends Worker {
 
+    private Context myContext;
+
     public RefreshDatabase(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+        this.myContext = context;
     }
 
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        refreshDatabase();
+        return Result.success();
+    }
+
+    private void refreshDatabase(){
+        SharedPreferences sharedPreferences = myContext.getSharedPreferences("com.begers.workmanagerjava", Context.MODE_PRIVATE);
+        int mySaveNumber = sharedPreferences.getInt("myNumber", 0);
+        mySaveNumber++;
+        System.out.println(mySaveNumber);
+        sharedPreferences.edit().putInt("myNumber", mySaveNumber).apply();
     }
 
 }
